bf014b5770c3c43b5e8881ca6958309ae8d40dbc
naoki2610 <n_ishizawa1970@outlook.jp>
2026-08-15 14:41:23 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['maplibre-gl'] // ← これを追加して地図の描画エラーを防ぎます
-  }
+  base: '/sakurajima-app/',  // 
 })
