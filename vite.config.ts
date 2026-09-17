d6e235b7c07a197f39cbd91a6afac1f6f9e6fcd9
naoki2610 <n_ishizawa1970@outlook.jp>
2026-09-17 16:51:12 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/sakurajima-app/',  // 
+  base: '/sakurajima-app/', // ←【超重要】この1行が絶対に必要です
 })
